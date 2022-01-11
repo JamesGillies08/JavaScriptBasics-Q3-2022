25f7bc4c16d0b13bdec19f666852de96a50b7b59
JamesGillies08 <j4mesgillies@gmail.com>
2022-01-11 16:18:47 -0700

    hi

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,13 @@ const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 600;
+
+console.log("Hello world");
+
+let suit = "Clubs";
+let face = "Ace";
+let value = "1";
+
+let isFace = true;
+
+let aceOfClubs = {}
